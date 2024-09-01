2d70a9f431a0eddd452d3f71b4cb9f39726be17c
Cixilation <marvellavians6@gmail.com>
2024-09-01 20:48:44 +0700

    feat : added login and register UI.

---
+++ app/src/main/java/edu/bluejack24_1/mysticvine/activities/LeaderBoardActivity.kt
@@ -0,0 +1,21 @@
+package edu.bluejack24_1.mysticvine.activities
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import edu.bluejack24_1.mysticvine.R
+
+class LeaderBoardPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_leaderboard)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
